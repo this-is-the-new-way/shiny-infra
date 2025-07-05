3791bd6858e3e2815207d166abc191481d406e2c
Neeraj B <forneeraj@gmail.com>
2025-07-05 18:20:36 -0400

    Remove dynamo db table

--- terraform.tf
+++ terraform.tf
@@ -14,8 +14,6 @@ terraform {
     bucket         = "terrastate-file"
     key            = "shiny-infra/terraform.tfstate"
     region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-state-lock"
   }
   #   encrypt        = true
   # }
